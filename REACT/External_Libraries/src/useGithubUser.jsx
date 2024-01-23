261b295bd116c2f29598db9339c7acc73566bd4e
Maria Cortes <mariacv60@hotmail.com>
2024-01-23 13:58:00 +0100

    Update useGithubUser.jsx

--- REACT/External_Libraries/src/useGithubUser.jsx
+++ REACT/External_Libraries/src/useGithubUser.jsx
@@ -1,23 +1,33 @@
 import useSWR from "swr";
 
 export function useGithubUser(username) {
- 
+  // Check if the username is null or undefined
+  if (username === null || username === undefined) {
+    return {
+      data: null,
+      error: null,
+      loading: false,
+      onFetchUser: () => {}, // No-op function if username is null
+    };
+  }
 
-const {data, error, mutate} =  useSWR(`${process.env.REACT_APP_SERVER_URL}/users/${username}`)
+  const { data, error, mutate } = useSWR(`${process.env.REACT_APP_SERVER_URL}/users/${username}`);
 
-function fetchGithubUser(){
-  mutate()
-}
+  function fetchGithubUser() {
+    mutate();
+  }
 
   return {
     data,
     error,
     loading: !data && !error,
-    onFetchUser : fetchGithubUser
+    onFetchUser: fetchGithubUser,
   };
 }
+
 
 
 // Ejercicios Custom Hooks 3 y 4
 // al configurar el fetcher en la root no lo necesitmaos mas aqui
 // podemos pasar en este segundo pasametro cualquier otra cosa tambien entre {}
+//Ejercicio SWR 1-2
